3180b0c2ff63f66dc8efc38a829623fccbae4a64
Kasper Karlsson <appekall@hotmail.com>
2015-09-30 18:31:09 +0200

    Updated comments

--- TeensyGamepad.ino
+++ TeensyGamepad.ino
@@ -7,6 +7,7 @@ https://github.com/kasperkarlsson/TeensyGamepad
 const int button_pins[] = {2, 3, 4, 5};
 // Button key mappings
 const char button_keys[] = {'W', 'A', 'S', 'D'};
+
 // Joystick pin mappings
 const int joystick_pins[] = {7, 8, 9, 10};
 // Joystick key mappings (arro)
@@ -19,6 +20,8 @@ const int number_of_joystick_pins = (sizeof(joystick_pins) / sizeof(int));
 int last_joystick_states[number_of_joystick_pins];
 int input_tmp;
 
+// Prints a status message to the Serial interface on the format
+// [Button | Joystick] <pin_number> [pressed|released]
 void printButtonStatus(boolean is_button, int index, boolean pressed) {
   if (is_button) {
     Serial.print("Button ");
@@ -36,21 +39,23 @@ void printButtonStatus(boolean is_button, int index, boolean pressed) {
 }
 
 void setup() {
+  // Initialize Serial interface
   Serial.begin(9600);
   Serial.println("Serial ready");
+  // Setup LED pin
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+  // Setup button pins
   Serial.println("Setting up pins and initializing");
   for (int i=0; i<number_of_buttons; i++) {
-    // Setup button pin
     pinMode(button_pins[i], INPUT_PULLUP);
-    // Initialize last state to avoid trigging on start
+    // Initialize last state to avoid triggering on start
     last_states[i] = HIGH;
   }
+  // Setup joystick pins
   for (int i=0; i<number_of_joystick_pins; i++) {
-    // Setup joystick pin
     pinMode(joystick_pins[i], INPUT_PULLUP);
-    // Initialize last state to avoid trigging on start
+    // Initialize last state to avoid triggering on start
     last_joystick_states[i] = HIGH;
   }
   Serial.println("Setup complete!");
@@ -60,7 +65,9 @@ void loop() {
   // Handle joystick functionality
   for (int i=0; i<number_of_joystick_pins; i++) {
     input_tmp = digitalRead(joystick_pins[i]);
+    // On joystick state change
     if (input_tmp != last_joystick_states[i]) {
+      // Light LED
       digitalWrite(LED_BUILTIN, last_joystick_states[i]);
       boolean is_low = input_tmp == LOW;
       printButtonStatus(false, i, is_low);
@@ -78,7 +85,9 @@ void loop() {
   // Handle button functionality
   for (int i=0; i<number_of_buttons; i++) {
     input_tmp = digitalRead(button_pins[i]);
+    // On button state change
     if (input_tmp != last_states[i]) {
+      // Light LED
       digitalWrite(LED_BUILTIN, last_states[i]);
       boolean is_low = input_tmp == LOW;
       printButtonStatus(true, i, is_low);
@@ -89,5 +98,6 @@ void loop() {
       last_states[i] = input_tmp;
     }
   }
+  // Delay between rounds
   delay(20);
 }
